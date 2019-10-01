27e88aa1dffc5e558c817caf86f2a0d92cc8f2d5
stupendous-patch-9345 <yo.park93@yahoo.com>
2019-10-01 19:15:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ function dwarfRollCall(dwarves) {
   var array = [];
   for(var i = 0; i < dwarves.length; i++) {
     array.push(`${i+1}. ${dwarves[i]} `);
-  } return array;
+  } return array.join("");
 }
